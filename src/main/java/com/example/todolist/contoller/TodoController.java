de86b29376534072c9a5f955b9c7eee35a47c775
woojin <tkdalsrb147@naver.com>
2025-08-11 14:48:55 +0900

    feat: create get controller

--- src/main/java/com/example/todolist/contoller/TodoController.java
+++ src/main/java/com/example/todolist/contoller/TodoController.java
@@ -5,10 +5,15 @@ import com.example.todolist.dto.TodoResponseDto;
 import com.example.todolist.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/todo")
 @RequiredArgsConstructor
 public class TodoController {
 
@@ -19,6 +24,13 @@ public class TodoController {
     return ResponseEntity.ok(TodoResponseDto.toDto(
         todoService.create(TodoRequestDto.toEntity(todoRequestDto))));
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<TodoResponseDto> read(@PathVariable Long id) {
+    return ResponseEntity.ok(TodoResponseDto.toDto(todoService.read(id)));
+  }
+
+
 
 
 }
